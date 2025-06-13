f8601c82dd950a379a7e5323bff1b6f435f340b6
Rishu <rishu02k@gmail.com>
2025-06-13 22:57:45 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -6,12 +6,46 @@ import cors from "cors";
 
 connectDB();
 
-app.use(
-  cors({
-    origin: [process.env.CLIENT_URL],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [process.env.CLIENT_URL],
+//     credentials: true,
+//   })
+// );
+
+const allowedOrigins = [process.env.CLIENT_URL];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps, curl)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+
+
+
+
+
+
+
+
 app.use(express.json());
 app.use(cookieParser());
 
